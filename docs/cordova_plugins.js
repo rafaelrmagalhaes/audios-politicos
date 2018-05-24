86b6aa075447aa787b601f5a9b2b1eb0e8195321
rafaelmagalhaess <rafaremagalhaes@gmail.com>
2018-05-24 00:16:33 -0300

    novos audios

--- docs/cordova_plugins.js
+++ docs/cordova_plugins.js
@@ -1,35 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.hutchind.cordova.plugins.streamingmedia/www/StreamingMedia.js",
-        "id": "com.hutchind.cordova.plugins.streamingmedia.StreamingMedia",
-        "pluginId": "com.hutchind.cordova.plugins.streamingmedia",
-        "clobbers": [
-            "streamingMedia"
-        ]
-    },
-    {
-        "file": "plugins/cordova-promise-polyfill/www/Promise.js",
-        "id": "cordova-promise-polyfill.Promise",
-        "pluginId": "cordova-promise-polyfill",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-promise-polyfill/www/promise.min.js",
-        "id": "cordova-promise-polyfill.promise.min",
-        "pluginId": "cordova-promise-polyfill"
-    },
-    {
-        "file": "plugins/cordova-plugin-admob-free/www/admob.js",
-        "id": "cordova-plugin-admob-free.AdMob",
-        "pluginId": "cordova-plugin-admob-free",
-        "clobbers": [
-            "admob",
-            "AdMob",
-            "plugins.AdMob"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "pluginId": "cordova-plugin-device",
@@ -79,38 +50,18 @@ module.exports = [
         "id": "cordova-plugin-statusbar.StatusBarProxy",
         "pluginId": "cordova-plugin-statusbar",
         "runs": true
-    },
-    {
-        "file": "plugins/es6-promise-plugin/www/promise.js",
-        "id": "es6-promise-plugin.Promise",
-        "pluginId": "es6-promise-plugin",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-        "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "pluginId": "cordova-plugin-x-socialsharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.hutchind.cordova.plugins.streamingmedia": "0.1.4",
-    "cordova-admob-sdk": "0.17.0",
-    "cordova-promise-polyfill": "0.0.2",
-    "cordova-plugin-admob-free": "0.17.0",
     "cordova-plugin-device": "2.0.2",
     "cordova-plugin-ionic-keyboard": "2.0.5",
     "cordova-plugin-ionic-webview": "1.2.1",
     "cordova-plugin-nativeaudio": "3.0.9",
     "cordova-plugin-splashscreen": "5.0.2",
     "cordova-plugin-statusbar": "2.4.2",
-    "cordova-plugin-whitelist": "1.3.3",
-    "es6-promise-plugin": "4.2.2",
-    "cordova-plugin-x-socialsharing": "5.4.0"
+    "cordova-plugin-whitelist": "1.3.3"
 }
 // BOTTOM OF METADATA
 });
