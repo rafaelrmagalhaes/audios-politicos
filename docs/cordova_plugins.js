8351aa4969f8ec0321bf75d83d2a6172c0c237fa
rafaelmagalhaess <rafaremagalhaes@gmail.com>
2018-05-24 00:02:58 -0300

    novos audios

--- docs/cordova_plugins.js
+++ docs/cordova_plugins.js
@@ -1,6 +1,35 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.hutchind.cordova.plugins.streamingmedia/www/StreamingMedia.js",
+        "id": "com.hutchind.cordova.plugins.streamingmedia.StreamingMedia",
+        "pluginId": "com.hutchind.cordova.plugins.streamingmedia",
+        "clobbers": [
+            "streamingMedia"
+        ]
+    },
+    {
+        "file": "plugins/cordova-promise-polyfill/www/Promise.js",
+        "id": "cordova-promise-polyfill.Promise",
+        "pluginId": "cordova-promise-polyfill",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-promise-polyfill/www/promise.min.js",
+        "id": "cordova-promise-polyfill.promise.min",
+        "pluginId": "cordova-promise-polyfill"
+    },
+    {
+        "file": "plugins/cordova-plugin-admob-free/www/admob.js",
+        "id": "cordova-plugin-admob-free.AdMob",
+        "pluginId": "cordova-plugin-admob-free",
+        "clobbers": [
+            "admob",
+            "AdMob",
+            "plugins.AdMob"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "pluginId": "cordova-plugin-device",
@@ -13,6 +42,15 @@ module.exports = [
         "id": "cordova-plugin-device.DeviceProxy",
         "pluginId": "cordova-plugin-device",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio-browser.js",
+        "id": "cordova-plugin-nativeaudio.NativeAudioBrowser",
+        "pluginId": "cordova-plugin-nativeaudio",
+        "clobbers": [
+            "cordova.plugins.NativeAudio",
+            "plugins.NativeAudio"
+        ]
     },
     {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
@@ -29,24 +67,50 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio-browser.js",
-        "id": "cordova-plugin-nativeaudio.NativeAudioBrowser",
-        "pluginId": "cordova-plugin-nativeaudio",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
         "clobbers": [
-            "cordova.plugins.NativeAudio",
-            "plugins.NativeAudio"
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    },
+    {
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "id": "es6-promise-plugin.Promise",
+        "pluginId": "es6-promise-plugin",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3",
+    "com.hutchind.cordova.plugins.streamingmedia": "0.1.4",
+    "cordova-admob-sdk": "0.17.0",
+    "cordova-promise-polyfill": "0.0.2",
+    "cordova-plugin-admob-free": "0.17.0",
     "cordova-plugin-device": "2.0.2",
-    "cordova-plugin-splashscreen": "5.0.2",
-    "cordova-plugin-ionic-webview": "1.2.1",
     "cordova-plugin-ionic-keyboard": "2.0.5",
-    "cordova-plugin-nativeaudio": "3.0.9"
+    "cordova-plugin-ionic-webview": "1.2.1",
+    "cordova-plugin-nativeaudio": "3.0.9",
+    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.3",
+    "es6-promise-plugin": "4.2.2",
+    "cordova-plugin-x-socialsharing": "5.4.0"
 }
 // BOTTOM OF METADATA
 });
---
+++ docs/plugins/cordova-promise-polyfill/www/Promise.js
@@ -0,0 +1,5 @@
+cordova.define("cordova-promise-polyfill.Promise", function(require, exports, module) { if (typeof window !== 'undefined' && !window.Promise) {
+  window.Promise = require('./promise.min')
+}
+
+});
