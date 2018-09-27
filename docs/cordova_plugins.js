9fd4d4a9ccf9174272ec869c2526c2839e471be2
rafaelmagalhaess <rafaremagalhaes@gmail.com>
2018-09-26 22:39:43 -0300

    atualizando versao web

--- docs/cordova_plugins.js
+++ docs/cordova_plugins.js
@@ -15,15 +15,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio-browser.js",
-        "id": "cordova-plugin-nativeaudio.NativeAudioBrowser",
-        "pluginId": "cordova-plugin-nativeaudio",
-        "clobbers": [
-            "cordova.plugins.NativeAudio",
-            "plugins.NativeAudio"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
         "pluginId": "cordova-plugin-splashscreen",
@@ -38,30 +29,24 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
+        "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio-browser.js",
+        "id": "cordova-plugin-nativeaudio.NativeAudioBrowser",
+        "pluginId": "cordova-plugin-nativeaudio",
         "clobbers": [
-            "window.StatusBar"
+            "cordova.plugins.NativeAudio",
+            "plugins.NativeAudio"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
-        "id": "cordova-plugin-statusbar.StatusBarProxy",
-        "pluginId": "cordova-plugin-statusbar",
-        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-device": "2.0.2",
-    "cordova-plugin-ionic-keyboard": "2.0.5",
-    "cordova-plugin-ionic-webview": "1.2.1",
-    "cordova-plugin-nativeaudio": "3.0.9",
     "cordova-plugin-splashscreen": "5.0.2",
-    "cordova-plugin-statusbar": "2.4.2",
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-ionic-webview": "1.2.1",
+    "cordova-plugin-ionic-keyboard": "2.1.3",
+    "cordova-plugin-nativeaudio": "3.0.9"
 }
 // BOTTOM OF METADATA
 });
--- docs/plugins/cordova-promise-polyfill/www/Promise.js
+++
@@ -1,5 +0,0 @@
-cordova.define("cordova-promise-polyfill.Promise", function(require, exports, module) { if (typeof window !== 'undefined' && !window.Promise) {
-  window.Promise = require('./promise.min')
-}
-
-});
